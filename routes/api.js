0ba83d013c774b11066ceb67c4b2d1ba1fb094d3
Emre Gül <48657990+emregull@users.noreply.github.com>
2022-01-17 21:22:50 +0300

    app.js eklendi controllers oluşturuldu

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,8 @@
 const express=require("express")
+const user_endpoint = require("./user/user");
 
-let router=express.Router()
+let router=express.Router();
+
+router.use("user", user_endpoint);
 
 module.exports=router;
