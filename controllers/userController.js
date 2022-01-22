a15271f778e5cda83010dec63695319804e20eb2
Emre Gül <48657990+emregull@users.noreply.github.com>
2022-01-22 22:56:50 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,19 +38,19 @@ exports.login = (req,res) => {
 }
 
 exports.register  = async (req, res, next) => { 
-    const schema = Joi.object({
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
+    const schema = Joi.object({   //There is making the schema for register validations
+        email: Joi.string().email().required(),  // Email parameter must be an email
+        password: Joi.string().min(6).required(), // Password parameter must be higher than 6 character
     });
     const { error, value } = schema.validate(req.body);
 
-    if (error) {
+    if (error) { //If validations are not correct then we are giving this status and message
         let response = {
             status: false,
             message: 'Email or password is wrong'
         }
         res.status(401).send(response)
-    } else {
+    } else { // If validations are correct then status true
         let response = {
             status: true
         }
